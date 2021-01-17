56b5c8e1797cae51a51b1fb3ec43c6bdc259dd2d
SkylixMC <aseef.imran@grandtheftmc.net>
2021-01-17 12:17:03 -0500

    Initial commit

---
+++ src/main/java/skylix/me/bungeecommandexecutor/bukkit/BungeeCommandExecutor.java
@@ -0,0 +1,17 @@
+package skylix.me.bungeecommandexecutor.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BungeeCommandExecutor extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/skylix/me/bungeecommandexecutor/bungee/BungeeCommandExecutor.java
@@ -0,0 +1,17 @@
+package skylix.me.bungeecommandexecutor.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeeCommandExecutor extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
